5ed3d18a646c3042898e8ed43153913ef14fda87
Igor Maculan <n3wtron@gmail.com>
2019-11-26 19:40:46 +0100

    removed useless functions

--- internal/utils/crypto.go
+++ internal/utils/crypto.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha512"
 	"crypto/x509"
 	"encoding/pem"
 	"github.com/sirupsen/logrus"
@@ -12,25 +11,6 @@ import (
 	"log"
 	"testing"
 )
-
-func BytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
-	block, _ := pem.Decode(priv)
-	enc := x509.IsEncryptedPEMBlock(block)
-	b := block.Bytes
-	var err error
-	if enc {
-		log.Println("is encrypted pem block")
-		b, err = x509.DecryptPEMBlock(block, nil)
-		if err != nil {
-			return nil, err
-		}
-	}
-	key, err := x509.ParsePKCS1PrivateKey(b)
-	if err != nil {
-		return nil, err
-	}
-	return key, nil
-}
 
 func BytesToPublicKey(pub []byte) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode(pub)
@@ -53,35 +33,6 @@ func BytesToPublicKey(pub []byte) (*rsa.PublicKey, error) {
 		return nil, err
 	}
 	return key, nil
-}
-
-func EncryptWithPublicKey(msg []byte, pubKey *rsa.PublicKey) ([]byte, error) {
-	hash := sha512.New()
-	cipherText, err := rsa.EncryptOAEP(hash, rand.Reader, pubKey, msg, nil)
-	if err != nil {
-		return nil, err
-	}
-	return cipherText, nil
-}
-
-func DecryptWithPrivateKey(cipherText []byte, privKey *rsa.PrivateKey) ([]byte, error) {
-	hash := sha512.New()
-	plaintext, err := rsa.DecryptOAEP(hash, rand.Reader, privKey, cipherText, nil)
-	if err != nil {
-		return nil, err
-	}
-	return plaintext, nil
-}
-
-func PrivateKeyToBytes(priv *rsa.PrivateKey) []byte {
-	privBytes := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(priv),
-		},
-	)
-
-	return privBytes
 }
 
 func PublicKeyToBytes(pub *rsa.PublicKey) []byte {
